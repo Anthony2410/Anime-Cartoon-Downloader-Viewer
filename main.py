fe634489bbefca27b1f1a6b93a2fa29a96c0f853
Anthony2410 <jaddawkins21@gmail.com>
2019-01-19 17:06:00 -0500

    Added helper function for downloading files

--- main.py
+++ main.py
@@ -17,6 +17,57 @@ from selenium import webdriver
 from pathlib import Path
 from bs4 import BeautifulSoup as BS
 
+def get_headers():
+    '''
+    '''
+    return {
+        'User-Agent': ''
+    }
+
+def get_html(*args, **kwargs):
+    '''
+    '''
+    try:
+        if kwargs.get('js'):
+            with webdriver.Firefox() as driver:
+                driver.get(args[0])
+                if kwargs.get('obj'):
+                    return BS(driver.page_source, 'html.parser')
+                else:
+                    return driver.page_source
+        else:
+            r = requests.get(args[0], headers=get_headers())
+            if r.status_code == 200:
+                if kwargs.get('obj'):
+                    return BS(r.text, 'html.parser')
+                else:
+                    return r.text
+    except Exception as e:
+        print(e)
+
+def get_image(*args):
+    '''
+    '''
+    try:
+        r = requests.get(args[0], headers=get_headers())
+        if r.status_code == 200:
+            return Image.open(BytesIO(r.content))
+    except Exception as e:
+        print(e)
+
+def download_file(*args, **kwargs):
+    '''
+    '''
+    try:
+        r = requests.get(args[0], headers=get_headers(), stream=True)
+        if r.status_code == 200:
+            with open(kwargs.get('file'), 'wb') as f:
+                for chunk in r.iter_content(
+                    chunk_size=kwargs.get('chuck_size', 128)):
+                    f.write(chunk)
+    except Exception as e:
+        print(e)
+
 
 
 class Root:
